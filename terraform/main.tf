cfbabffa6e6f7a77fe3fbd9e4cc293f581ce38ba
Guilherme Medeiros <gacmedeiros@gmail.com>
2025-07-29 17:55:53 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-variable "project_id" {
-  type = string
-}
-
-variable "region" {
-  type    = string
-  default = "us-central1"
-}
